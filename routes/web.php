4e3f04c93e8035d14ee626ad0597978d58fdc9f3
kishor <kishor@myhealthcare.life>
2025-05-24 12:57:39 +0530

    welcome

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 // Route::get('/clear-cache', function() {
 //     Artisan::call('cache:clear');
 //     Artisan::call('view:clear');
